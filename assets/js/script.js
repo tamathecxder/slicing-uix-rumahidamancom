b9d9ef734cb1ff25c3d21798cea4f18107a7d294
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-06-19 11:44:51 +0700

    Me-restructure kode html dan menambahkan styling media query untuk bagian hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
 
   if ( window.scrollY > '80' ) {
     navbar.classList.replace('bg-transparent', 'nav-color');
